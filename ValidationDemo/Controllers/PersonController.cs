40a42f168359e84f73cc51a2dc8eee095758f9d7
OneJ <3942241@qq.com>
2017-03-19 02:30:57 +0800

    校验

---
+++ ValidationDemo/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ValidationDemo.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ ValidationDemo/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ValidationDemo.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "不能为空")]
+        [Range(10,100,ErrorMessage = "只能输入10-100")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
